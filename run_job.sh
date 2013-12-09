d70720bc64fd86e7224c8449e04366d64c4bb020
Dawei Wang <aneyas@gmail.com>
2013-12-09 11:28:57 +0800

    Restart always fail. So back to from-scratch.
    Here is the error message.
    
         Self-consistent Calculation
    
         iteration #  1     ecut=    40.00 Ry     beta=0.15
         Davidson diagonalization with overlap
         c_bands:  3 eigenvalues not converged
    
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         Error in routine cdiaghg (211):
         S matrix not positive definite
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

--- run_job.sh
+++ run_job.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-nohup /opt/mpich_icc_ifort/bin/mpirun -np 8 ~/software/PWSCF/espresso-5.0.3/bin/pw.x -npool 8 -input YBCO_STO.pw.in &>YBCO_STO.pw.out &
+npool=1
+
+nohup /opt/mpich_icc_ifort/bin/mpirun -np 1 ~/software/PWSCF/espresso-5.0.3/bin/pw.x -nimage 1 -npool $npool -input YBCO_STO.pw.in &>YBCO_STO.pw.out &
